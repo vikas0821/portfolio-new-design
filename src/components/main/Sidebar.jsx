7e83e975600b64140273a6fc725cf19989e4df41
vikas0821 <vikaskannaujiya207@gmail.com>
2023-06-06 18:50:59 +0530

    here i've added new icons

--- src/components/main/Sidebar.jsx
+++ src/components/main/Sidebar.jsx
@@ -5,11 +5,11 @@ function Sidebar() {
   return (
     <div className='flex flex-col justify-evenly h-4/5 h- text-2xl text-left pl-5 text-white-100 absolute top-4'>
     <Link to="/home"><div className=' bg-extradarkcream-800 w-48 rounded-lg pl-3 py-2'> <i class="px-2 fa-solid fa-house"></i> Home</div></Link> 
-     <Link to="/about"><div className=' bg-extradarkcream-800 w-48 rounded-lg pl-3 py-2'> <i class="px-2 fa-solid fa-address-book"></i>About me</div></Link> 
-     <Link to="/education"><div className=' bg-extradarkcream-800 w-48 rounded-lg pl-3 py-2'> <i class="px-2 fa-solid fa-address-book"></i>Education</div></Link> 
-     <Link to="/projects"> <div className=' bg-extradarkcream-800 w-48 rounded-lg pl-3 py-2'><i class="px-2 fa-solid fa-address-book"></i>Project</div></Link>
-     <Link to="/skills"><div className=' bg-extradarkcream-800 w-48 rounded-lg pl-3 py-2'> <i class="px-2 fa-solid fa-address-book"></i>Skills</div></Link> 
-     <Link to="/certificate"><div className=' bg-extradarkcream-800 w-48 rounded-lg pl-3 py-2'><i class="px-2 fa-solid fa-address-book"></i>Certificates</div></Link> 
+     <Link to="/about"><div className=' bg-extradarkcream-800 w-48 rounded-lg pl-3 py-2'> <i class="px-2 fa-solid fa-user"></i> About me</div></Link> 
+     <Link to="/education"><div className=' bg-extradarkcream-800 w-48 rounded-lg pl-3 py-2'> <i class="px-2 fa-solid fa-graduation-cap"></i>Education</div></Link> 
+     <Link to="/projects"> <div className=' bg-extradarkcream-800 w-48 rounded-lg pl-3 py-2'><i class="px-2 fa-sharp fa-solid fa-diagram-project"></i>Project</div></Link>
+     <Link to="/skills"><div className=' bg-extradarkcream-800 w-48 rounded-lg pl-3 py-2'> <i class="px-2 fa-solid fa-code"></i>Skills</div></Link> 
+     <Link to="/certificate"><div className=' bg-extradarkcream-800 w-48 rounded-lg pl-3 py-2'><i class="px-2 fa-solid fa-certificate"></i>Certificates</div></Link> 
      <Link to="/contact"><div className=' bg-extradarkcream-800 w-48 rounded-lg pl-3 py-2'> <i class="px-2 fa-solid fa-address-book"></i>Contact</div></Link> 
     </div>
   )
